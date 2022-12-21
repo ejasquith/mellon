590ed0f286cb8b7ddc8b0681fc0dbe61ac7bee4a
ejasquith <emily.asquith@icloud.com>
2022-12-21 14:08:16 +0000

    Register post and comment models to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('body', 'author', 'created_on',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('body', 'author', 'post', 'created_on')
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Post(models.Model):
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
