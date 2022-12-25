82b40246063c253dbc94dd68bc927e6769c2f23e
ejasquith <emily.asquith@icloud.com>
2022-12-25 15:25:55 +0000

    Implement create post modal

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.db.models import Q
+
 from .models import Post
+from accounts.models import CustomUser as User
 
 class PostList(generic.ListView):
     model = Post
