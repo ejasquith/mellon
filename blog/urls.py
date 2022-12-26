13e7fc8af71f2e5db10eaa70422b1fb7f29ec679
ejasquith <emily.asquith@icloud.com>
2022-12-26 14:43:35 +0000

    Implement create post functionality from form

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('create_post/', views.CreatePost.as_view(), name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.db.models import Q
 from django.utils import timezone
+from django.http import JsonResponse
 
 from .models import Post
 from accounts.models import CustomUser as User
@@ -15,3 +16,12 @@ class PostList(generic.ListView):
     # by user - will be ordered by date in template
     queryset = Post.objects.order_by('author', '-created_on').distinct('author')
     template_name = 'home.html'
+
+
+class CreatePost(View):
+    def post(self, request):
+        data = request.POST
+        body = data.get('body')
+        author = request.user
+        post = Post.objects.create(body=body, author=author)
+        return JsonResponse({'status':'success'})
