d405e18f0fe9ecca301f2aa31b7e69b58c4c9244
ejasquith <emily.asquith@icloud.com>
2022-12-29 00:11:27 +0000

    Add login_required to relevant views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('user/<slug:slug>', ProfileView.as_view(), name='profile'),
-    path('user/<slug:slug>/edit', EditProfileView.as_view(), name='edit_profile'),
+    path('user/<slug:slug>', login_required(ProfileView.as_view()), name='profile'),
+    path('user/<slug:slug>/edit', login_required(EditProfileView.as_view()), name='edit_profile'),
     path('add_friend', AddFriendView.as_view(), name='add_friend'),
-    path('friends', FriendsListView.as_view(), name='friends_list'),
+    path('friends', login_required(FriendsListView.as_view()), name='friends_list'),
     path('accept_friend_request', AcceptFriendshipView.as_view(), name='accept_friend_request'),
     path('reject_friend_request', RejectFriendshipView.as_view(), name='reject_friend_request'),
     path('cancel_friend_request', CancelFriendshipView.as_view(), name='cancel_friend_request'),
     path('remove_friend', RemoveFriendView.as_view(), name='remove_friend'),
     path('check_friend_requests', CheckFriendRequestsView.as_view(), name='check_friend_requests'),
-    path('find_friends', FindFriendsView.as_view(), name='find_friends')
+    path('find_friends', login_required(FindFriendsView.as_view()), name='find_friends')
 ]
